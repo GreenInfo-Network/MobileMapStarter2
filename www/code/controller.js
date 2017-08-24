2973effc6c8a1643ebeb327e75447d4bec70e4cb
Greg Allensworth <you@example.com>
2017-08-24 12:02:49 -0700

    #2 rework page-turning (again) / add mapsettings.html and links

--- www/code/controller.js
+++ www/code/controller.js
@@ -1,4 +1,16 @@
-export const APP_CONTROLLER = function ($scope, SharedState) {
+export const APP_CONTROLLER = function ($scope) {
+    //
+    // the currently-visible page, so only one of the panels is visible at a time
+    // see selectPage() and use it in ng-click directives, to link between panels
+    // ngRoute and ui.router have some undesirable behaviors such as lazy-loading and auto-unloading
+    // which are poison for maps and other expensive operations
+    // and mobile-angular-ui's SharedState service doesn't propagate to $scope to work with ng-switch for example, without more workarounds
+    //
+    $scope.selectedPage = 'welcome';
+    $scope.selectPage = function (which) {
+        $scope.selectedPage = which;
+    };
+
     //
     // initialization: a Leaflet Map in $scope.map
     // has to be done asynchronously since Angular may not have loaded map.html yet
@@ -17,7 +29,7 @@ export const APP_CONTROLLER = function ($scope, SharedState) {
             }).addTo($scope.map);
 
             // watch for a page change to 'map' so we can fix Leaflet's hatred of being invisible
-            $scope.$on('mobile-angular-ui.state.changed.whichPage', function(e, newVal, oldVal) {
+            $scope.$watch('selectedPage', function(newVal, oldVal) {
                 if (newVal !== 'map') return;
                 setTimeout(function () {
                     $scope.map.invalidateSize();
